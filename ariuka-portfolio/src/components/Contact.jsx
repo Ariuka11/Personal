47aa003c508c2f7ea2f4419f964bd42ce49b9cfa
ariuka11 <ariun1118@gmail.com>
2022-02-18 23:12:25 -0500

    added contact section

--- ariuka-portfolio/src/components/Contact.jsx
+++ ariuka-portfolio/src/components/Contact.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import wave3 from "../images/wave3.svg";
 
 const Contact = () => {
   return (
     <div className="contact-container">
-      <div className="right">Right</div>
-      <div className="left">Left</div>
+      <div className="right">
+        <img src={wave3} alt="" />
+      </div>
+      <div className="left">
+        <h2>Skills</h2>
+      </div>
     </div>
   );
 };
